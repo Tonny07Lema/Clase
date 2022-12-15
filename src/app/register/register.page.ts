669df6d39d14d62e784435679bd06594423313d3
Tonny07Lema <tonnylema09@gmail.com>
2022-12-15 08:35:48 -0500

    first commit

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { UserService } from '../api/user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  userRegisterForm : FormGroup = this.fb.group({      
+    'username' : ['', [Validators.required]],
+    'password': ['', [Validators.required]],
+    'confirmPassword': ['', [Validators.required]]
+  });   
+
+  constructor(
+    private fb: FormBuilder , 
+    private userService: UserService,
+    private toastController : ToastController
+    ) { }
+
+  ngOnInit() {
+   
+  }
+
+  register(){
+    if(!this.userRegisterForm.valid){
+      return false;
+    }else{
+      this.userService.register(this.userRegisterForm.value)
+      .subscribe(
+        (data) =>{
+          console.log('Hola',data);
+        },
+        (error)=> {
+          console.log('Ocurrio un error',error.error)
+          this.mostrarMensaje (error.error)
+        });
+      return true;
+    }
+  }
+
+  async mostrarMensaje(mensaje :any){
+    const toast = await this.toastController.create({
+      position : 'top',
+      message : mensaje,
+      duration : 3000
+    });
+    toast.present();
+  }
+}
